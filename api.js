60784844dee288471bc1474fe898dff8bc73a4d1
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-08-22 14:01:09 -0300

    updates documentation with BaseObject and Events

--- api.js
+++ api.js
@@ -1,12 +1,19 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "BaseObject",
+        "Events",
         "Player"
     ],
     "modules": [
+        "base",
         "components"
     ],
     "allModules": [
+        {
+            "displayName": "base",
+            "name": "base"
+        },
         {
             "displayName": "components",
             "name": "components"
